af3a3f841908d573716dffd9e51827c760ec8127
carlos-e-strella <caaaaaaaaaaaarlos@gmail.com>
2025-12-15 20:56:51 -0500

    Added transient weak form calculation for single element.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 #  MeshSolve
-A c rewrite of my python pde solver. Will be implemented as a python package with a new interface.
+A c rewrite of my python pde solver. Will be implemented as a python package with a new interface. 
+I am not a mathematician nor an engineer, so this program is probably packed with errors.
+
+## How does this program solve PDEs
+I have implemented (barely) the finite element method, which is a numerical method to aproximate solutions for PDEs.
+The method requires you to specify a mesh where your equations occurr and calculates a stiffness matrix for it.
+The solution then involves solving a linear system involving said matrix and your initial conditions (and your boundary conditions).
+
+## How does this program implement the finite element method?
+This program provides functions to calculate the stiffness matrix for a specific mesh.
+Once its finished, you will be able to declare your partial differential equation as a sum of different terms.
+Each term has a weak form that is used to calculate its contribution to the stiffness matrix.
+Finally, the program will calculate the resulting solution by solving a system of equations involving said matrix.
--- src/meshsolve.c
+++ src/meshsolve.c
@@ -8,13 +8,30 @@
 double* array_reference[128];
 int array_index = 0;
 
-
-// TODO What happens when you have more than 128 arrays???
-double* array_constructor(size_t size) {
-    double* new_array = (double*) malloc(size * sizeof(double));
-    for (int i = 0; i < size; i++) {
-        new_array[i] = 0;
+// Printing for matrices in column major order (what cblas uses)
+void print_matrix(double* matrix, int sizex, int sizey) {
+    for (int i = 0; i < sizey; i++) {
+        for (int j = 0; j < sizex; j++) {
+            printf("%.2f ", matrix[i + j*sizey]);
+        }
+        printf("\n");
     }
+}
+
+double* create_array(size_t size) {
+    double* memory = (double*) malloc(size * sizeof(double));
+    if (!memory) {
+        printf("Error during memory allocation");
+        return NULL;
+    }
+    for (int i = 0; i < size; i++) {
+        memory[i] = 0;
+    }
+    return memory;
+}
+
+double* array_constructor(size_t size) {
+    double* new_array = create_array(size);
     array_reference[0] = new_array;
     array_index++;
     return new_array;
@@ -25,6 +42,11 @@ void array_destructor() {
         free(array_reference[i]);
         array_reference[i] = NULL;
     }
+}
+
+// Apparently openblas does not have this?
+double determinant_2d(double* matrix) {
+    return fabs(matrix[0] * matrix[3] - matrix[1] * matrix[2]);
 }
 
 // Used for conversion from pyvista meshes to elements
@@ -65,6 +87,22 @@ double* grad_shape_funcs_quad(double xi, double eta) {
     return grad_shape_funcs;
 }
 
+// Jacobian of the transformation from quadrilateral reference element
+double* jacobian_quad(double* element, double* gradient) {
+    static double jacobian[4];
+    double* dxi; dxi = &(gradient[0]);
+    double* deta; deta = &(gradient[1]);
+    double* x; x = &(element[0]);
+    double* y; y = &(element[1]);
+
+    jacobian[0] = cblas_ddot(4, x, 2, dxi, 2);
+    jacobian[1] = cblas_ddot(4, x, 2, deta, 2);
+    jacobian[2] = cblas_ddot(4, y, 2, dxi, 2);
+    jacobian[3] = cblas_ddot(4, y, 2, deta, 2);
+
+    return jacobian;
+}
+
 // Integration with gauss-legendre quadrature
 // TODO add more order options
 double* quad_integral(double* (*integrand)(double), int shape_num) {
@@ -73,7 +111,7 @@ double* quad_integral(double* (*integrand)(double), int shape_num) {
     // Weights for 2 points are 1, 1 soo...
     
     // This here will definitely exceed 128 arrays
-    double* quadrature = array_constructor(shape_num);
+    double* quadrature = create_array(shape_num);
     for (int i = 0; i < order; i++) {
         double* integral = integrand(gauss_points[i]);
         cblas_daxpy(shape_num, 1, integral, 1, quadrature, 1);
@@ -88,15 +126,52 @@ double* dbl_quad_integral(double* (*integrand)(double, double), int shape_num) {
     double gauss_points[2] = {-1/sqrt(3), 1/sqrt(3)};
     // Weights for 2 points are 1, 1 soo...
 
-    double* quadrature = array_constructor(shape_num);
+    double* quadrature = create_array(shape_num);
     for (int i = 0; i < order; i++) {
         for (int j = 0; j < order; j++) {
             double* integral = integrand(gauss_points[i], gauss_points[j]);
             cblas_daxpy(shape_num, 1, integral, 1, quadrature, 1);
+            free(integral); integral = NULL;
         }
     }
 
     return quadrature;
 }
 
+// The actually useful part
+// TODO add triangular elements
+double quad_element_buffer[4];
+
+// Integral for transient term ∂T/∂t on a quadrilateral element
+double* transient_integrand(double x, double y) {
+    // Size four arrays
+    double* shape_funcs = shape_funcs_quad(x, y);
+    double* gradient = grad_shape_funcs_quad(x, y); 
+
+    // 2x2 matrix
+    double* jacobian = jacobian_quad(quad_element_buffer, gradient);
+    double determinant = determinant_2d(jacobian);
+
+    // 4x4 element matrix
+    // N ⊗ N for each quadrature point
+    double* integrand = create_array(16);
+    cblas_dger(CblasColMajor, 4, 4, determinant, shape_funcs, 1, shape_funcs, 1, integrand, 4);
+    return integrand;
+}
+
+// Wrapper for transient elements, only for quadrilateral elements
+double* transient_wrapper(double* element, int shape_num) {
+    memcpy(quad_element_buffer, element, 4 * sizeof(double));
+    double* element_stiffness = dbl_quad_integral(transient_integrand, shape_num * shape_num);
+    return element_stiffness;
+}
+
 // TODO ensure safety when passing the number of shape functions
+int main() {
+    double test_element[8] = {8.0, 2.0, 5.0, 6.0, 1.0, 2.0, 8.0, 9.0};
+    double* matrix = transient_wrapper(test_element, 4);
+    print_matrix(matrix, 4, 4);
+    free(matrix); matrix = NULL;
+
+    return 0;
+}
